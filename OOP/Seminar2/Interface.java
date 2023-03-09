374b940b8174ff660fbaf9de108a40cf97177c61
Arseniy <zubkov_000@list.ru>
2023-03-09 20:04:29 +0300

    Задача с шкалой здоровья и  усталости

---
+++ OOP/Seminar2/Interface.java
@@ -0,0 +1,24 @@
+package OOP.Seminar2;
+
+interface HasHealthPoint {
+
+    int getMaxHealthPoint();
+
+    int getCurrentHealthPoint();
+
+    int randomHP();
+}
+
+interface Tiredness {
+    int getMaxHealthPoint();
+
+    int getCurrentHealthPoint();
+
+    int getMaxEnergy();
+
+    int getCurrentEnergy();
+
+    int randomHP();
+    
+    int randomEnergy();
+}
